60de85e17994d2bc7520ca7b40732cfc6b849b03
hclee <heechul4296@gmail.com>
2023-03-01 14:09:01 +0900

    initial source

---
+++ src/main/java/study/springboot/springbootautoconfiguration/SpringbootAutoconfigurationApplication.java
@@ -0,0 +1,13 @@
+package study.springboot.springbootautoconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAutoconfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAutoconfigurationApplication.class, args);
+    }
+
+}
